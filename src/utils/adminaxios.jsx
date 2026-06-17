20576b006a85d9823ea9df53db2597d079c880a3
Justice3984 <90700124+Justice3984@users.noreply.github.com>
2026-06-17 16:08:55 +0100

    Update adminaxios.jsx

--- src/utils/adminaxios.jsx
+++ src/utils/adminaxios.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const ADMIN_BASE_URL = "https://stay-next-admin-service.onrender.com/api/admin";
 =======
 const AUTH_BASE_URL = "https://stay-next-auth-service-4.onrender.com/api";
@@ -11,7 +10,7 @@ export const API = axios.create({
   baseURL: AUTH_BASE_URL,
   withCredentials: true,
 });
->>>>>>> 8b1d509582446aa0ec269aea294c995dbdf3860d
+
 
 // 📊 Instance for hitting Admin Services (e.g., /agents)
 export const AdminAPI = axios.create({
@@ -19,32 +18,29 @@ export const AdminAPI = axios.create({
   withCredentials: true,
 });
 
-<<<<<<< HEAD
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers["Authorization"] = `Bearer ${token}`;
   }
 
-=======
 // Attach interceptor to BOTH instances to guarantee headers are appended dynamically
 const injectToken = (config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
->>>>>>> 8b1d509582446aa0ec269aea294c995dbdf3860d
+
   if (!(config.data instanceof FormData)) {
     config.headers["Content-Type"] = "application/json";
   }
   return config;
 };
 
-<<<<<<< HEAD
+
 export default API;
 =======
 API.interceptors.request.use(injectToken, (err) => Promise.reject(err));
 AdminAPI.interceptors.request.use(injectToken, (err) => Promise.reject(err));
 
 export default API;
->>>>>>> 8b1d509582446aa0ec269aea294c995dbdf3860d
